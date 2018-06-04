d303e1f3434ff8b728fb1680857e43e942123b54
Alexandr Denysenko <denysenkoa@digicode.ua>
2018-06-04 15:40:49 +0300

    clear repository

--- app/reducers/Test.js
+++
@@ -1,7 +0,0 @@
-const initialState = {
-  items:[]
-};
-
-const test = (state = initialState) => state;
-
-export default test;
--- app/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import test from './Test';
-
-
-const rootReducer = combineReducers({
-    test
-});
-
-export default rootReducer;
